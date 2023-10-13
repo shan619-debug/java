f536a3fc3afbe8e2b8fc6603cb2223be3b2a8547
shan619-debug <devopsnhc.012@gmail.com>
2023-10-13 11:28:07 +0530

    my first commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum  
+{  
+public static void main(String args[])   
+{  
+int n1 = 100, n2 = 150, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
